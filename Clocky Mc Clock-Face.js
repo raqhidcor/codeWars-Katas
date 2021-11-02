d4248f01d98d30c57e49d2a74a3a59de9986922b
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-03 00:01:52 +0100

    Kata completed

---
+++ Clocky Mc Clock-Face.js
@@ -0,0 +1,39 @@
+// Story
+// Due to lack of maintenance the minute-hand has fallen off Town Hall clock face.
+
+// And because the local council has lost most of our tax money to a Nigerian email scam there are no funds to fix the clock properly.
+
+// Instead, they are asking for volunteer programmers to write some code that tell the time by only looking at the remaining hour-hand!
+
+// What a bunch of cheapskates!
+
+// Can you do it?
+
+// Kata
+// Given the angle (in degrees) of the hour-hand, return the time in HH:MM format. Round down to the nearest minute.
+
+// Examples
+// 12:00 = 0 degrees
+// 03:00 = 90 degrees
+// 06:00 = 180 degrees
+// 09:00 = 270 degrees
+// 12:00 = 360 degrees
+
+var whatTimeIsIt = function(angle) {
+    let mins = angle * 2
+    let h = Math.floor(mins / 60)
+    let m = Math.floor(mins % 60);
+    if(h === 0){
+      h = '12'
+    }
+    if(h < 10){
+      h = `0${h}`
+    }
+    if(m < 10){
+      m = `0${m}`
+    }
+    return `${h}:${m}`
+    }
+
+
+    // You have passed all of the tests! :)
