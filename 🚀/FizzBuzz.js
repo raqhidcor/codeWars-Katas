e70adf11d8604a829c99544c69670cff2907374d
Raquel Hidalgo <raqhidcor@gmail.com>
2022-01-15 11:20:34 +0100

    FizzBuzz test completed

---
+++ 🚀/FizzBuzz.js
@@ -0,0 +1,19 @@
+//Given a number n, for each integer i in the range from 1 to n inclusive, print one value per line as follows:
+
+//-If i is a multiple of both 3 and 5, print FizzBuzz
+//-If is a multiple of 3 (but not 5), print Fizz
+//-If is a multiple of 5 (but not 3), print Buzz
+//-If is not a multiple of 3 or 5, print the value of i 
+
+function fizzBuzz(n) {
+    for(let i=1; i<=100; i++){
+        if (i % 3 == 0 && i % 5 == 0){
+            console.log('FizzBuzz')
+        } else if ( i % 3 == 0){
+            console.log('Fizz')
+        } else if (i % 5 == 0){
+            console.log('Buzz')
+        } else {
+            console.log(i)
+        }
+    }}
